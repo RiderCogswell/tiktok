ed82ffe2828b4e12dd0f52add5607685fe80ca04
Ridercogswell <ridercogswell@gmail.com>
2022-10-18 14:33:08 -0700

    create schemas

---
+++ briktok-backend/schemas/post.js
@@ -0,0 +1,46 @@
+export default {
+  name: 'post', 
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'caption',
+      title: 'Caption',
+      type: 'string'
+    },
+    {
+      name: 'video',
+      title: 'Video',
+      type: 'file',
+      options: {
+        hotspot: true,
+      }
+    },
+    {
+      name: 'userId',
+      title: 'UserId',
+      type: 'string', 
+    },
+    {
+      name: 'postedBy',
+      title: 'Posted By',
+      type: 'postedBy', 
+    },
+    {
+      name: 'likes',
+      title: 'Likes', 
+      type: 'array',
+      of: [
+        {
+          type: 'references',
+          to: [{ type: 'user' }],
+        }
+      ]
+    },
+    {
+      name: 'topic',
+      title: 'Topic',
+      type: 'string', 
+    },
+  ]
+}

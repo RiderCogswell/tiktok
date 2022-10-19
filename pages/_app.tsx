76124597dcfd849f11fe453803fcc2236715c0d3
Ridercogswell <ridercogswell@gmail.com>
2022-10-19 00:06:07 -0700

    create components and add tailwind styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState, useEffect } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Navbar } from '../components/Navbar'
+
+import { Sidebar } from '../components/Sidebar'
+import '../styles/globals.css'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR , setIsSSR] = useState(true); 
+
+  useEffect(() => { // if we come across this code, it means that we are using react, which is Client Side Rendered, so we set isSSR to false
+    setIsSSR(false);
+  }, []) // only run at the start so leave dependency array empty
+
+  if (isSSR) return null;// we create this base case so that if we are server side rendering, we do not want to show our components
+
+  return (
+    <div>
+      <Navbar />      
+      <div className='flex gap-6 md:gap-20'>
+        <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+          <Sidebar />
+        </div>
+        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1 '>
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default MyApp

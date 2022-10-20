f46687a2c376831a00ff99ae7a2d6158397fd45e
Ridercogswell <ridercogswell@gmail.com>
2022-10-19 18:52:01 -0700

    adding utils

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <Link>
+        <div>
+          <Image
+            className='cursor-pointer'
+          />
+        </div>
+      </Link>
+    </div>
   )
 }

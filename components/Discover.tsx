13ff5582e1ec26b0adc9794cc5152c2fb0bd4cae
Ridercogswell <ridercogswell@gmail.com>
2022-10-23 19:57:37 -0700

    style categories

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -6,15 +6,25 @@ import { topics } from '../utils/constants'
 
 
 const Discover = () => {
+  const activeTopicStyle = 'xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black';
+  const topicStyle = 'xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black';
+
   return (
     <div className='xl:border-b-2 xl:border-gray-200 pb-6'>
-      <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>
+      <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'> 
         Popular Topics
-      </p>
-      <div>
-        {topics.map((items) => (
-          <Link href={`/?topics/${}`}>
-
+      </p> 
+      <div className='flex gap-3 flex-wrap'>
+        {topics.map((topic) => (
+          <Link href={`/?topic=${topic.name}`} key={topic.name}>
+            <div className={topicStyle}>
+              <span className='font-bold text-2xl xl:text-md'>
+                {topic.icon}
+              </span>
+              <span className='hidden xl:block capitalize font-medium text-md '>
+                {topic.name}
+              </span>
+            </div>
           </Link>
         ))}
       </div>

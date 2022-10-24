f53f2dd315d62692d8f38c405f6bd4ea41347c4c
Ridercogswell <ridercogswell@gmail.com>
2022-10-23 22:10:38 -0700

    finish footer styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
+import { footerList1, footerList2, footerList3 } from '../utils/constants'
+
+export const List = ({ items } : { items: string[] }) => ( 
+  <div className='flex flex-wrap gap-2 mt-5'>
+    {items.map((item) => (
+      <p key={item} className="text-gray-400 text-sm hover:underline cursor-pointer">
+        {item}
+      </p>
+    ))}
+  </div>
+)
+
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='mt-6 hidden xl:block'>
+      <List items={footerList1} />
+      <List items={footerList2} />
+      <List items={footerList3} />
+      <p className='text-gray-200 text-sm mt-5'>2022 Rider&apos;s TikTok</p>
+    </div>
   )
 }

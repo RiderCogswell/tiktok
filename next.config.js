7ce32b20fc646af745e83b1b0d287a420f34f68a
Ridercogswell <ridercogswell@gmail.com>
2022-10-27 14:59:07 -0700

    declare image domain in config file + add video card

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['i2-prod.walesonline.co.uk']
+  }
 }
 
 module.exports = nextConfig
+
+// nextjs requires our paths to be declared before we can take things from cdn's

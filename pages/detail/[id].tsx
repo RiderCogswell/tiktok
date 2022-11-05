c169b1dcf2fbe1e8d6ad38bef61f921f3b94a7e7
Ridercogswell <ridercogswell@gmail.com>
2022-11-05 00:31:32 -0700

    detail page renders

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -15,9 +15,35 @@ interface IProps {
 }
 
 const Detail = ({ postDetails } : IProps) => {
-  return (
-    <div>
+  const [post, setPost] = useState(postDetails)
+  
 
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  if (!post) return null;
+
+  return (
+    <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
+      <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center'>
+        <div className='absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
+          <p>
+            <MdOutlineCancel className='text-white text-[35px]' />
+          </p>
+          <div className='relative'>
+            <div className='lg:h-[100vh] h-[60vh]'>
+              <video 
+                ref={videoRef}
+                loop
+                onClick={() => {}}
+                src={post.video.asset.url}
+                className='cursor-pointer h-full'
+              >
+
+              </video>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

69a52cf3ec71669e95296f0465c736b6cc29dd51
Ridercogswell <ridercogswell@gmail.com>
2022-11-08 15:20:46 -0800

    typos

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -53,7 +53,7 @@ const Detail = ({ postDetails } : IProps) => {
 
   return (
     <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
-      <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-cover bg-center'>
+      <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center'>
         <div className='absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
           <p className='cursor-pointer' onClick={() => router.back()}> 
             <MdOutlineCancel className='text-white text-[35px]' />

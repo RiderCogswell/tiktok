8e56bc42169b8611b9a14065b17c2b3bbe3b6e2f
Ridercogswell <ridercogswell@gmail.com>
2022-11-13 14:48:38 -0800

    use the Dispatch<SetStateAction> combo type with a val of string to describe setting comment = to string

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Dispatch, SetStateAction } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { GoVerified } from 'react-icons/go'
@@ -9,7 +9,7 @@ import { NoResults } from './NoResults'
 interface IProps {
   isPosting: boolean;
   comment: string;
-  setComment: () => void;
+  setComment: Dispatch<SetStateAction<string>>;
   addComment: () => void;
   comments: IComment[];
 }
@@ -27,7 +27,7 @@ const Comments = ({comment, setComment, addComment, isPosting, comments }: IProp
   return (
     <div className='border-r-2 border-gray-200 pt-4 px-10 bg-[#f8f8f8] border-b-2 lg:pb-0 pb-[100px]'>
       <div className='overflow-scroll lg:h-[475px]'>
-        {comments.length ? (
+        {comments?.length ? (
           <div>videos</div>
         ) : (
           <NoResults text='Be the first to comment!' />
@@ -38,7 +38,7 @@ const Comments = ({comment, setComment, addComment, isPosting, comments }: IProp
             <form onSubmit={() => {}} className='flex gap-4'>
               <input 
                 className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg' 
-                onChange={() => {}}
+                onChange={(e) => setComment(e.target.value.trim())}
                 placeholder='Add a comment...'
               />
               <button className='text-md text-gray-400' onClick={() => {}}>

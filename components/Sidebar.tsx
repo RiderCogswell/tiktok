3dff39f0c25cb43fc2eb66ee6670129a06175e5e
Ridercogswell <ridercogswell@gmail.com>
2022-11-14 15:27:02 -0800

    fixed bottom padding issue

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -19,7 +19,7 @@ export const Sidebar = () => {
   return (
     <div>
         <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r border-gray-100 xl:border-0 p-3'>
-          <div className='xl:border-b-0 border-gray-200 xl:pb-4'>
+          <div className='xl:border-b-0 border-gray-200 '>
             <Link href='/'>
               <div className={normalLink}>
                 <p className='text-2xl'>

5a7987fa640e34ec1336e115f2b3daafb7fb2212
Ridercogswell <ridercogswell@gmail.com>
2024-06-19 17:00:03 -0700

    minor fix

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -60,7 +60,7 @@ const Detail = ({ postDetails } : IProps) => {
     }
   }
   
-  const addComment = async (e: React.FormEvent) => {
+  const addComment = async (e: { preventDefault(): () => void }) => {
     e.preventDefault();
     
     if (userProfile && comment) { 
